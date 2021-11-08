70de82b39a4b5d170ce0cabf9f5a4b560a5c706d
crazywhalecc <crazywhalecc@163.com>
2021-11-08 16:49:49 +0800

    update readme and demo

--- src/OneBot/V12/Action/ReplAction.php
+++ src/OneBot/V12/Action/ReplAction.php
@@ -6,7 +6,7 @@ use OneBot\Console\Console;
 use OneBot\V12\Object\ActionObject;
 use OneBot\V12\Utils;
 
-class ReplAction
+class ReplAction extends ActionBase
 {
     public function onSendMessage(ActionObject $action): ActionResponse {
         Console::success(Utils::msgToString($action->params['message']));
