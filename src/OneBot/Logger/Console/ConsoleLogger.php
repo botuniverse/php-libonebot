39ac43cfce931c4fcf3a17aefa4254e65d77dd78
sunxyw <xy2496419818@gmail.com>
2021-11-18 18:03:16 +0800

    cleanup unnecessary codes in ConsoleLogger

--- src/OneBot/Logger/Console/ConsoleLogger.php
+++ src/OneBot/Logger/Console/ConsoleLogger.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace OneBot\Logger\Console;
 
 use OneBot\Util\Singleton;
-use Psr\Log\LoggerInterface;
+use Psr\Log\AbstractLogger;
+use Psr\Log\InvalidArgumentException;
 use Psr\Log\LogLevel;
 
-class ConsoleLogger implements LoggerInterface
+class ConsoleLogger extends AbstractLogger
 {
     use Singleton;
 
@@ -120,50 +121,15 @@ class ConsoleLogger implements LoggerInterface
         return '';
     }
 
-    public function emergency($message, array $context = [])
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    public function alert($message, array $context = [])
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    public function critical($message, array $context = [])
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    public function error($message, array $context = [])
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    public function warning($message, array $context = [])
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    public function notice($message, array $context = [])
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    public function info($message, array $context = [])
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    public function debug($message, array $context = [])
-    {
 //        $trace = debug_backtrace()[1] ?? ['file' => '', 'function' => ''];
 //        $trace = '[' . ($trace['class'] ?? '') . ':' . ($trace['function'] ?? '') . '] ';
-        $this->log(LogLevel::DEBUG, $message, $context);
-    }
 
     public function log($level, $message, array $context = [])
     {
+        if (!in_array($level, self::$levels)) {
+            throw new InvalidArgumentException();
+        }
+
         if (array_flip(self::$levels)[$level] > self::$logLevel) {
             return;
         }
