98486000f836a90e0a921e626510dcb91e58a1cd
sunxyw <xy2496419818@gmail.com>
2021-11-20 16:05:19 +0800

    fix missing arg in PrivateMessageEvent

--- src/OneBot/V12/Object/Event/Message/PrivateMessageEvent.php
+++ src/OneBot/V12/Object/Event/Message/PrivateMessageEvent.php
@@ -14,13 +14,14 @@ use OneBot\V12\Object\MessageSegment;
 class PrivateMessageEvent extends MessageEvent
 {
     /**
+     * @param string                                 $user_id 用户 ID
      * @param MessageSegment|MessageSegment[]|string $message 消息内容
      * @param null|DateTimeInterface|int             $time    事件发生时间，可为DateTime对象或时间戳，不传或为null则使用当前时间
      *
      * @throws OneBotException
      */
-    public function __construct($message, $time = null)
+    public function __construct(string $user_id, $message, $time = null)
     {
-        parent::__construct('private', $message, $time);
+        parent::__construct('private', $user_id, $message, $time);
     }
 }
