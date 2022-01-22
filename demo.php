a00196b76125e5e52e16505429780a54684fa927
crazywhalecc <jesse2061@outlook.com>
2022-01-22 20:39:33 +0800

    update demo

--- demo.php
+++ demo.php
@@ -7,7 +7,7 @@ require_once 'vendor/autoload.php';
 $ob = new \OneBot\V12\OneBot('repl', 'qq', 'REPL-1');
 $ob->setLogger(new \OneBot\Logger\Console\ConsoleLogger());
 $ob->setDriver(
-    new \OneBot\V12\Driver\WorkermanDriver(),
+    new \OneBot\Driver\WorkermanDriver(),
     new \OneBot\V12\Config\Config('demo.json')
 );
 $ob->setActionHandler(\OneBot\V12\Action\ReplAction::class);
