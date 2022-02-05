7fb6fca04051d49220f3ee9bd67f64be9bdd7d20
sunxyw <xy2496419818@gmail.com>
2022-02-05 22:28:44 +0800

    add unit test for Config

---
+++ test/unit/ConfigTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OneBot\V12\Config;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \OneBot\V12\Config\Config
+ */
+class ConfigTest extends TestCase
+{
+    /**
+     * @covers \OneBot\V12\Config\Config::get
+     */
+    public function testGetCanReturnExactValue(): void
+    {
+        $this->assertEquals('good', $this->getConfig()->get('top', 'bad'));
+    }
+
+    /**
+     * @covers \OneBot\V12\Config\Config::get
+     */
+    public function testGetCanReturnDefaultValue(): void
+    {
+        $this->assertEquals('bad', $this->getConfig()->get('middle', 'bad'));
+    }
+
+    /**
+     * @covers \OneBot\V12\Config\Config::get
+     */
+    public function testGetCanReturnDeepValue(): void
+    {
+        $this->assertEquals('good', $this->getConfig()->get('bottom.left', 'bad'));
+    }
+
+    private function getConfig(): Config
+    {
+        return new Config([
+            'top' => 'good',
+            'bottom' => [
+                'left' => 'good',
+                'right' => 'good',
+            ],
+        ]);
+    }
+}
