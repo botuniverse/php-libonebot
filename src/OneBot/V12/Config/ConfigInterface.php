761b591ce74db8911bdc539f0b8a95cf6a3a8c9b
sunxyw <xy2496419818@gmail.com>
2022-02-10 22:07:27 +0800

    add set method to Config

--- src/OneBot/V12/Config/ConfigInterface.php
+++ src/OneBot/V12/Config/ConfigInterface.php
@@ -16,6 +16,14 @@ interface ConfigInterface
     public function get(string $key, $default = null);
 
     /**
+     * 设置配置项
+     *
+     * @param string     $key   键名，使用.分割多维数组
+     * @param null|mixed $value 值，null表示删除
+     */
+    public function set(string $key, $value): void;
+
+    /**
      * 获取启用的通讯方式
      */
     public function getEnabledCommunications(): array;
