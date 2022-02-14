b85c8cbdd5f72cc5fd020e7b79d8a643dd06e9d4
Jerry Ma <jesse2061@outlook.com>
2022-02-14 13:04:33 +0800

    move ExceptionHandler.php to Driver

--- src/OneBot/Driver/ExceptionHandler.php
+++ src/OneBot/Driver/ExceptionHandler.php
@@ -1,8 +1,11 @@
 <?php
 
+/** @noinspection PhpUndefinedClassInspection */
+/* @noinspection PhpUndefinedNamespaceInspection */
+
 declare(strict_types=1);
 
-namespace OneBot\Logger\Console;
+namespace OneBot\Driver;
 
 use OneBot\Util\Singleton;
 use Throwable;
@@ -27,9 +30,6 @@ class ExceptionHandler
         }
     }
 
-    /**
-     * @return null|\Whoops\Run
-     */
     public function getWhoops()
     {
         return $this->whoops;
@@ -41,7 +41,7 @@ class ExceptionHandler
     public function handle(Throwable $e): void
     {
         if (is_null($this->whoops)) {
-            ob_logger()->error($e->getMessage());
+            ob_logger()->error('Uncaught ' . get_class($e) . ': ' . $e->getMessage() . ' at ' . $e->getFile() . '(' . $e->getLine() . ')');
             ob_logger()->error($e->getTraceAsString());
             return;
         }
