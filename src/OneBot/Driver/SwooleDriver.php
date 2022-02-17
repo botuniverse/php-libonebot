a9377065cf5bd88764a24af746d52cd343643414
crazywhalecc <jesse2061@outlook.com>
2022-02-17 19:58:02 +0800

    add user process for swoole driver and finish driver init event

--- src/OneBot/Driver/SwooleDriver.php
+++ src/OneBot/Driver/SwooleDriver.php
@@ -6,21 +6,24 @@ declare(strict_types=1);
 
 namespace OneBot\Driver;
 
+use OneBot\Driver\Event\DriverInitEvent;
 use OneBot\Driver\Event\EventDispatcher;
+use OneBot\Driver\Event\EventProvider;
 use OneBot\Driver\Event\Http\HttpRequestEvent;
 use OneBot\Driver\Event\Process\ManagerStartEvent;
 use OneBot\Driver\Event\Process\ManagerStopEvent;
+use OneBot\Driver\Event\Process\UserProcessStartEvent;
 use OneBot\Driver\Event\Process\WorkerStartEvent;
 use OneBot\Driver\Event\Process\WorkerStopEvent;
 use OneBot\Driver\Event\WebSocket\WebSocketCloseEvent;
 use OneBot\Driver\Event\WebSocket\WebSocketMessageEvent;
 use OneBot\Driver\Event\WebSocket\WebSocketOpenEvent;
 use OneBot\Driver\Interfaces\WebSocketClientInterface;
+use OneBot\Driver\Swoole\UserProcess;
 use OneBot\Http\HttpFactory;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 use Swoole\Http\Server as SwooleHttpServer;
-use Swoole\Process;
 use Swoole\Server;
 use Swoole\WebSocket\Frame;
 use Swoole\WebSocket\Server as SwooleWebSocketServer;
@@ -64,7 +67,7 @@ class SwooleDriver extends Driver
             }
         }
         if ($ws_index !== null) {
-            $this->server = new SwooleWebSocketServer($comm[$ws_index]['host'], $comm[$ws_index]['port']);
+            $this->server = new SwooleWebSocketServer($comm[$ws_index]['host'], $comm[$ws_index]['port'], $this->getParam('swoole_server_mode', SWOOLE_PROCESS));
             $this->initServer();
             if ($http_index !== null) {
                 ob_logger()->warning('检测到同时开启了http和正向ws，http的配置项将被忽略。');
@@ -73,7 +76,7 @@ class SwooleDriver extends Driver
             $this->initWebSocketServer();
         } elseif ($http_index !== null) {
             //echo "新建http服务器.\n";
-            $this->server = new SwooleHttpServer($comm[$http_index]['host'], $comm[$http_index]['port']);
+            $this->server = new SwooleHttpServer($comm[$http_index]['host'], $comm[$http_index]['port'], $this->getParam('swoole_server_mode', SWOOLE_PROCESS));
             $this->initHttpServer();
             $this->initServer();
         }
@@ -81,16 +84,38 @@ class SwooleDriver extends Driver
             $this->http_webhook_url = $comm[$has_http_webhook]['url'];
         }
 
-        $process = new Process(function () {
-            //while (true) {
-            /* @noinspection PhpComposerExtensionStubsInspection */
-            echo date('H:i:s') . ' 开启个人进程[' . posix_getpid() . ']！' . $this->server->worker_id . PHP_EOL;
-
-            sleep(2);
-        //}
-        }, false);
-        echo '牛逼' . PHP_EOL;
-        $this->server->addProcess($process);
+        switch ($this->getDriverInitPolicy()) {
+            case DriverInitPolicy::MULTI_PROCESS_INIT_IN_MASTER:
+            case DriverInitPolicy::MULTI_PROCESS_INIT_IN_ALL_PROCESSES:
+                $event = new DriverInitEvent($this);
+                (new EventDispatcher())->dispatch($event);
+                break;
+            case DriverInitPolicy::MULTI_PROCESS_INIT_IN_USER_PROCESS:
+                EventProvider::addEventListener(UserProcessStartEvent::getName(), function () {
+                    $event = new DriverInitEvent($this);
+                    (new EventDispatcher())->dispatch($event);
+                    if ($this->getParam('init_in_user_process_block', true) === true) {
+                        while (true) {
+                            sleep(100000);
+                        }
+                    }
+                }, 1);
+                break;
+        }
+        // 添加插入用户进程的启动仪式
+        if (!empty(EventProvider::getEventListeners(UserProcessStartEvent::getName()))) {
+            $process = new UserProcess(function () {
+                ProcessManager::initProcess(ONEBOT_PROCESS_USER, 0);
+                ob_logger()->debug('新建UserProcess');
+                try {
+                    $event = new UserProcessStartEvent();
+                    (new EventDispatcher())->dispatch($event);
+                } catch (Throwable $e) {
+                    ExceptionHandler::getInstance()->handle($e);
+                }
+            }, false);
+            $this->server->addProcess($process);
+        }
     }
 
     /**
@@ -174,10 +199,10 @@ class SwooleDriver extends Driver
      */
     private function initServer(): void
     {
-        $this->server->set([
+        $this->server->set($this->getParam('swoole_set', [
             'max_coroutine' => 300000,
             'max_wait_time' => 5,
-        ]);
+        ]));
         $this->server->on('workerstart', function (Server $server) {
             ProcessManager::initProcess(ONEBOT_PROCESS_WORKER, $server->worker_id);
             try {
