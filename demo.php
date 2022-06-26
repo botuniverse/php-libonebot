d10d7cfd7861d73c5c10ae9bf74f635013f60f66
crazywhalecc <jesse2061@outlook.com>
2022-06-26 23:58:42 +0800

    remove ReplAction default ActionHandler, just use demo

--- demo.php
+++ demo.php
@@ -1,40 +1,40 @@
 <?php
 
-declare(strict_types=1);
+/**
+ * @noinspection PhpFullyQualifiedNameUsageInspection
+ */
 
-use OneBot\V12\Config\Config;
-use OneBot\V12\OneBotBuilder;
+declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
 
+function message_id(): string
+{
+    return uniqid('', true);
+}
+
 $config = [
     'name' => 'repl',
-
     'platform' => 'qq',
-
     'self_id' => 'REPL-1',
-
     'db' => true,
-
     'logger' => [
         'class' => \OneBot\Logger\Console\ConsoleLogger::class,
-        'level' => 'debug',
+        'level' => 'info',
     ],
-
     'driver' => [
-        'class' => \OneBot\Driver\WorkermanDriver::class,
+        'class' => \OneBot\Driver\SwooleDriver::class,
         'config' => [
             // 'driver_init_policy' => \OneBot\Driver\DriverInitPolicy::MULTI_PROCESS_INIT_IN_USER_PROCESS,
             'init_in_user_process_block' => true,
-            'swoole_server_mode' => SWOOLE_BASE,
         ],
     ],
-
     'communications' => [
         [
             'type' => 'http',
             'host' => '127.0.0.1',
             'port' => 2345,
+            'worker_count' => 8,
             'access_token' => '',
             'event_enabled' => true,
             'event_buffer_size' => 100,
@@ -43,7 +43,7 @@ $config = [
             'type' => 'http_webhook',
             'url' => 'https://example.com/webhook',
             'access_token' => '',
-            'timeout' => 5,
+            'timeout' => 5000,
         ],
         [
             'type' => 'websocket',
@@ -55,25 +55,31 @@ $config = [
             'type' => 'ws_reverse',
             'url' => 'ws://127.0.0.1:20001',
             'access_token' => '',
-            'reconnect_interval' => 5,
+            'reconnect_interval' => 5000,
         ],
     ],
 ];
 
-// OneBotBuilder::factory()
-//    ->setName($config['name'])
-//    ->setPlatform($config['platform'])
-//    ->setSelfId($config['self_id'])
-//    ->useLogger($config['logger'])
-//    ->useDriver($config['driver'])
-//    ->setCommunicationsProtocol($config['communications'])
-//    ->build();
+const ONEBOT_APP_VERSION = '1.0.0-snapshot';
 
-// OneBotBuilder::buildFromConfig(new Config($config));
-
-$ob = OneBotBuilder::buildFromArray($config);
-$ob->setActionHandlerClass(\OneBot\V12\Action\ReplAction::class);
-$ob->addActionHandler('my_action', function (OneBot\V12\Object\ActionObject $obj) {
-    return \OneBot\V12\Action\ActionResponse::create($obj->echo)->ok(['hello' => 'world']);
+$ob = OneBot\V12\OneBotBuilder::buildFromArray($config); // 传入通信方式
+$ob->addActionHandler('send_message', function (OneBot\V12\Object\Action $obj) { // 写一个动作回调
+    \OneBot\Util\Utils::validateParamsByAction($obj, ['detail_type' => ['private']]); // 我这里只允许私聊动作，否则 BAD_PARAM
+    ob_logger()->info(OneBot\Util\Utils::msgToString($obj->params['message'])); // 把字符串转换为终端输入，因为这是 REPL 的 demo
+    return \OneBot\V12\Action\ActionResponse::create($obj->echo)->ok(['message_id' => message_id()]); // 返回消息回复
 });
+
+// 下面是一个简单的 REPL 实现，每次输入一行，就会触发一次 private.message 事件并通过设定的通信方式发送
+\OneBot\Driver\Event\EventProvider::addEventListener(\OneBot\Driver\Event\DriverInitEvent::getName(), function ($event) {
+    ob_logger()->info('Init 进程启动！' . $event->getDriver()->getName());
+    $event->getDriver()->addReadEvent(STDIN, function ($x) use ($event) {
+        $s = fgets($x);
+        if ($s === false) {
+            $event->getDriver()->delReadEvent($x);
+            return;
+        }
+        $event = new \OneBot\V12\Object\Event\Message\PrivateMessageEvent('tty', trim($s));
+        \OneBot\V12\OneBot::getInstance()->dispatchEvent($event);
+    });
+}, 0);
 $ob->run();
