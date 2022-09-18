695f0cc515ccbc3cc5f9819051b4112b51d2d00e
sunxyw <git@sunxyw.xyz>
2022-09-19 03:07:53 +0800

    add config class

---
+++ src/OneBot/Config/Config.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OneBot\Config;
+
+class Config
+{
+    /**
+     * @var RepositoryInterface 配置仓库
+     */
+    protected RepositoryInterface $repository;
+
+    /**
+     * 构造新的配置对象
+     *
+     * @param null|array|RepositoryInterface|string $context 配置数组、配置文件路径或是配置仓库，留空表示后续手动设置
+     */
+    public function __construct($context = null)
+    {
+        switch (true) {
+            case is_array($context):
+                $this->repository = new Repository($context);
+                break;
+            case is_string($context):
+                $this->repository = new Repository(require $context);
+                break;
+            case $context instanceof RepositoryInterface:
+                $this->repository = $context;
+                break;
+            default:
+                $this->repository = new Repository();
+        }
+    }
+
+    /**
+     * 获取配置仓库
+     */
+    public function getRepository(): RepositoryInterface
+    {
+        return $this->repository;
+    }
+
+    /**
+     * 设置配置仓库
+     */
+    public function setRepository(RepositoryInterface $repository): void
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * 获取配置项
+     *
+     * @param  string           $key     键名，使用.分割多维数组
+     * @param  mixed            $default 默认值
+     * @return null|array|mixed
+     */
+    public function get(string $key, $default = null)
+    {
+        return $this->repository->get($key, $default);
+    }
+
+    /**
+     * 设置配置项
+     *
+     * @param string     $key   键名，使用.分割多维数组
+     * @param null|mixed $value 值，null表示删除
+     */
+    public function set(string $key, $value): void
+    {
+        $this->repository->set($key, $value);
+    }
+
+    /**
+     * 判断配置项是否存在
+     *
+     * @param  string $key 键名，使用.分割多维数组
+     * @return bool   是否存在
+     */
+    public function has(string $key): bool
+    {
+        return $this->repository->has($key);
+    }
+
+    /**
+     * 获取所有配置项
+     */
+    public function all(): array
+    {
+        return $this->repository->all();
+    }
+}
